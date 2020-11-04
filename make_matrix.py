bb1e743ece8bc70eadb5f787cc4205d304d4329c
Alexander Koller <koller@ling.uni-potsdam.de>
2020-11-04 10:50:00 +0100

    solvable SLEs

---
+++ make_matrix.py
@@ -0,0 +1,60 @@
+import numpy as np
+from numpy.linalg import *
+
+# Methods for generating linear algebra problems, cf. Steele 1997.
+
+max_val = 3
+
+def mk_det_matrix(n, r):
+    """Generates a random n x n matrix with determinant +/- r and integer coefficients.
+    Special case: r = 1 => inverse has only integer coefficients
+    Special case: r = 0 => generate linearly dependent vectors
+    """
+    
+    L = np.tril(np.random.randint(-max_val, max_val, size=(n,n)))
+    np.fill_diagonal(L, ones(n)) # set diagonal entries to values with product 1
+
+    U = np.triu(np.random.randint(-max_val, max_val, size=(n,n)))
+    np.fill_diagonal(U, ones(n)) # set diagonal entries to values with product 1
+
+    m = np.random.randint(n)
+    L[m,m] = r
+
+    P = mk_random_permutation_matrix(n)
+
+    ret = P @ L @ U
+    return ret
+        
+def mk_random_permutation_matrix(n):
+    "Generate a random n x n permutation matrix."
+    ret = np.zeros((n,n), dtype='int64')
+    perm = np.random.permutation(n)
+    ret[np.arange(n), perm] = 1
+    return ret
+
+def im(A):
+    "Convert A into an integer matrix"
+    return np.round(A).astype('int64')
+
+def ones(n):
+    "Returns vector of +/- 1 values of length n that multiply to 1"
+    x = (2 * np.random.randint(0,2,size=n)) - 1
+    return (x / np.prod(x)).astype('int64')
+
+
+def make_unique_sle(n):
+    A = mk_det_matrix(n,1)
+    solution = np.random.randint(-max_val, max_val, size=n)
+    b = A @ solution
+    return A,b
+
+# A, b = make_unique_sle(3)
+# x = np.linalg.solve(A,b)
+# print(A)
+# print(b)
+# print(x)
+
+
+# A = mk_det_matrix(3,1)
+# print(A)
+# print(np.linalg.det(A))
---
+++ web.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from make_matrix import *
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/linear-equations.html')
+def linear():
+    A, b = make_unique_sle(3)
+    x = np.linalg.solve(A,b).astype('int64')
+
+    formatted = [[format_coefficient(A, row, col) for col in range(A.shape[1])] for row in range(A.shape[0])]
+
+    xparts = [str(coeff) for coeff in x]
+    solution = f"x = ({', '.join(xparts)})"
+
+
+    return render_template("linear-equations.html", A=A, b=b, formatted=formatted, solution=solution)
+
+
+def format_coefficient(A, row, col):
+    "Returns tuple (op, coefficient, variable)."
+    variable = f"x_{col+1}"
+
+    if A[row,col] == 0:
+        return "", "", ""
+
+    elif abs(A[row,col]) == 1:
+        op = "" if col == 0 else signop(A[row,col])
+        return op, "", variable
+
+    else:
+        op = "" if col == 0 else signop(A[row,col])
+        coeff = abs(A[row,col])
+        return op, coeff, variable
+
+
+def signop(value):
+    return "-" if value < 0 else "+"
+
+
+
+app.run(debug=True)
